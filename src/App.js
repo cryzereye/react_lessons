087ab78120dd2b65f6dcfd9ac3035573a7b35aef
MISYSROOT\jecarlos <JerichoFrancis.Carlos@finastra.com>
2019-03-26 19:45:49 +0800

    before eject

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import React, { Component } from 'react';
 import './App.css';
+//import Radium, { StyleRoot } from "radium";
 import Person from './Person/Person';
 
 class App extends Component {
@@ -62,11 +63,16 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      /*':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }*/
     }
 
     let persons = null;
@@ -84,23 +90,38 @@ class App extends Component {
             })}
           </div>
         )
+        style.backgroundColor = 'red';
+        /*style[':hover'] = {
+          backgroundColor: 'salmon',
+          color: 'black'
+        }*/
       };
 
+    let classes = [];
+    if (this.state.persons.length <= 2){
+      classes.push('red'); // push is array appened
+    }
+
+    if (this.state.persons.length <= 1){
+      classes.push('bold');
+    }
+
     return (
-      // only 1 root element for JSX
-      <div className="App">
-        <h1>Hi, I'm a React app</h1>
-        <p>This is really working!</p>
-        <button
-          onClick={() => this.togglePersonsHandler()}
-          style={style} >
-          {this.state.showPersons === true ? 'Hide' : 'Show'} Persons </button>
-          { persons }
-      </div>
+      //<StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React app</h1>
+          <p className = {classes.join(' ')}> This is really working!</p>
+          <button
+            onClick={() => this.togglePersonsHandler()}
+            style={style} >
+            {this.state.showPersons === true ? 'Hide' : 'Show'} Persons </button>
+            { persons }
+        </div>
+      //</StyleRoot>
     );
     // JSX is translated to below:
     //return React.createElement('div', {className: 'App'},  React.createElement('h1', null, 'Does this work know?'));
   }
 }
 
-export default App;
+export default App //Radium(App);
