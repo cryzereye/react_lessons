36465515ff61f5f3c17d69066a7848803f4b5d3d
MISYSROOT\jecarlos <JerichoFrancis.Carlos@finastra.com>
2019-03-26 09:15:40 +0800

    start of vid52

--- src/App.js
+++ src/App.js
@@ -9,32 +9,76 @@ class App extends Component {
       { name: 'Jecho', age: 24},
       { name: 'Vaughn', age: 24},
       { name: 'Japo', age: 23}
-    ]
+    ],
+    otherStart: 'nothing',
+    showPersons: false
   }
 
-switchNameHandler = () => {
-  //console.log("nice");
-  // NEVER DO THIS: this.state.persons[0].name = "Jericho Francis";
-  this.setState({
-    persons: [
-      { name: 'Jericho Francis', age: 24},
-      { name: 'Vaughn', age: 24},
-      { name: 'Japo', age: 23}
-    ]
-  });
-}
+  switchNameHandler = () => {
+    //console.log("nice");
+    // NEVER DO THIS: this.state.persons[0].name = "Jericho Francis";
+    this.setState({
+      persons: [
+        { name: 'Jericho Francis', age: 24},
+        { name: 'Vaughn', age: 24},
+        { name: 'Japo', age: 23}
+      ]
+    });
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Jecho', age: 24},
+        { name: event.target.value, age: 24},
+        { name: 'Japo', age: 23}
+      ]
+    });
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({
+      showPersons: !doesShow
+    });
+  }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+          <div>
+            <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+            <Person 
+              name={this.state.persons[1].name}
+              age={this.state.persons[1].age}
+              click = {this.switchNameHandler.bind(this, 'Jecho')}
+              changed = {this.nameChangedHandler}> and my hobby is nothing! </Person>
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+          </div>
+        )
+      };
+
     return (
       // only 1 root element for JSX
       <div className="App">
         <h1>Hi, I'm a React app</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> and my hobby is nothing! </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-     </div>
+        <button
+          onClick={() => this.togglePersonsHandler()}
+          style={style} >
+          {this.state.showPersons === true ? 'Hide' : 'Show'} Persons </button>
+          { persons }
+      </div>
     );
     // JSX is translated to below:
     //return React.createElement('div', {className: 'App'},  React.createElement('h1', null, 'Does this work know?'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,6 +5,7 @@ const person = (props) => {
         <div>
             <p>I'm a {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
+            <input type='text' onChange = {props.changed}/>
         </div>
     )
 }
